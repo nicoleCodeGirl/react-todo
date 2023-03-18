74ad9cb82c83596559732c0d40b00138ee28d42f
nicoleCodeGirl <nicolecodegirl@gmail.com>
2023-03-18 12:07:18 -0700

    edited app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
+const todoList = [
+  { id: 1, title: 'Complete assignment' },
+  { id: 2, title: 'Buy groceries' },
+  { id: 3, title: 'Go for a run' },
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+    <h1>Todo List</h1>
+    <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+  </div>
+
   );
 }
 
