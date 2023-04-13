a97962a65d85c29a96e9a2c5102bd801be15a4d6
nicoleCodeGirl <nicolecodegirl@gmail.com>
2023-04-12 19:39:46 -0700

    submitting lesson 1-4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import TodoList from "./TodoList";
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('');
-  return (
+// const [newTodo, setNewTodo] = useState('');
+const [todoList, setTodoList] = useState([]); // add a new state variable for todoList
+
+function addTodo (newTodo){
+  setTodoList([...todoList, newTodo]); // call setTodoList and use the spread operator to add newTodo to the existing todoList array
+  }
+
+return (
 
    <div>
      <h1>ToDo List</h1>
-     <AddTodoForm onAddTodo={setNewTodo}/>
-     <p>{newTodo}</p>
-     <TodoList />
+     <AddTodoForm onAddTodo={addTodo}/>
+     {/* <p>{}</p> */}
+     <TodoList todoList={todoList} /> {/* pass todoList state as a prop named todoList to the TodoList component */}
    </div>
 
   );
