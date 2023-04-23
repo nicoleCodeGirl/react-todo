8cea1fc833be2dcab131e9357f16ad77cebc8ee6
nicoleCodeGirl <nicolecodegirl@gmail.com>
2023-04-23 16:46:42 -0700

    submitting lesson 1-5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from './AddTodoForm';
 
-function App() {
-// const [newTodo, setNewTodo] = useState('');
-const [todoList, setTodoList] = useState([]); // add a new state variable for todoList
+// Above the App functional component, create a new function named useSemiPersistentState which will be a custom hook
+function useSemiPersistentState() {
+  // Cut (copy/remove) the useState and useEffect hooks from App into useSemiPersistentState
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")) || []);
 
-function addTodo (newTodo){
-  setTodoList([...todoList, newTodo]); // call setTodoList and use the spread operator to add newTodo to the existing todoList array
+  // define a useEffect hook with todoList as a dependency
+  useEffect(() => {
+    // inside the side-effect handler function, save the todoList inside localStorage with the key "savedTodoList"
+    // Update your side-effect function to convert todoList to a string before saving in localStorage
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]); // specify todoList as a dependency of the useEffect hook
+
+  // Add a return statement in useSemiPersistentState that returns the todoList state variable and setter in an Array (just like how it's returned from the useState hook)
+  return [todoList, setTodoList];
+}
+
+function App() {
+  // Update App to use the new custom hook
+  const [todoList, setTodoList] = useSemiPersistentState();
+
+  function addTodo (newTodo){
+    setTodoList([...todoList, newTodo]);
   }
 
-return (
-
-   <div>
-     <h1>ToDo List</h1>
-     <AddTodoForm onAddTodo={addTodo}/>
-     {/* <p>{}</p> */}
-     <TodoList todoList={todoList} /> {/* pass todoList state as a prop named todoList to the TodoList component */}
-   </div>
-
+  return (
+    // update the JSX to use a Fragment
+    <>
+      <h1>ToDo List</h1>
+      <AddTodoForm onAddTodo={addTodo}/>
+      <TodoList todoList={todoList} />
+    </>
   );
 }
 
